17bb95876d0b8273a777a847b6108dc4b544497b
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-11-26 21:15:48 -0500

    Dummy cards now in separate component

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,7 +1,6 @@
 // js
 import {React, useState, useRef} from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Container from 'react-bootstrap/Container';
@@ -11,7 +10,9 @@ import Alert from 'react-bootstrap/Alert';
 
 
 
-import Error from '../error/Error.jsx'
+import VideoCards from './VideoCards'
+import Error from '../error/Error.jsx';
+import dummySearch from '/Users/moniquecorrea/pursuit/module-3/youtube-clone/src/data/video.js';
 
 
 export default function Videos(){
@@ -21,12 +22,12 @@ export default function Videos(){
     const [videosList, setVideos] = useState([]);
     const videos = useRef([])
     // const [cards, setCards] = useState([]);
-    let videoList = [];
+    const videoList = [];
+    const {items} = dummySearch;
 
 
     function errorDisplay(error){
         setModalShow(true);
-        // console.log(error.error.code, error.error.message);
         setVideos({code: error.error.code, message: error.error.message});
     }
 
@@ -34,47 +35,57 @@ export default function Videos(){
     function getVideoQuery(event){
         event.preventDefault();
 
-            fetch(
-              `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${userSearch}&key=${process.env.REACT_APP_API_KEY}`,
-              {
-                method: "GET",
-              }
-            )
-              .then((response) => response.json())
-              .then((data) => {
-                if (videos.current.length > 0)  videos.current.length = 0;
-                !!data.items ? data.items.forEach((movie) => videos.current.push(movie)) : errorDisplay(data);
-                // if (data.items.length > 0) data.items.forEach((movie) => videos.current.push(movie))
-                console.log(data);
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-              });
-              videoList = [...videos.current]
-              console.log(userSearch)
-         videoList.map((item) => {
-            return (
-            <>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.snippet.thumbnails.default} />
-            <Card.Body>
-              <Card.Title>{item.snippet.title}</Card.Title>
-              <Card.Text>
-                {item.snippet.description}
-              </Card.Text>
-              <Button variant="danger">Watch!</Button>
-            </Card.Body>
-          </Card>
-          </>) 
-          } 
-          )
+            // fetch(
+            // //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${userSearch}&key=${process.env.REACT_APP_API_KEY}`,
+            // {dummySearch},
+            //   {
+            //     method: "GET",
+            //   }
+            // )
+        //       .then((response) => response.json())
+        //       .then((data) => {
+        //         if (videos.current.length > 0)  videos.current.length = 0;
+        //         !!data.items ? data.items.forEach((movie) => videos.current.push(movie)) : errorDisplay(data);
+        //         // if (data.items.length > 0) data.items.forEach((movie) => videos.current.push(movie))
+        //         console.log(data);
+        //       })
+        //       .catch((error) => {
+        //         console.error("Error:", error);
+        //       });
+        //       videoList = [...videos.current]
+        //       console.log(userSearch)
+        //  videoList.map((item) => {
+        //     return (
+        //     <>
+        //     <Card style={{ width: '18rem' }}>
+        //     <Card.Img variant="top" src={item.snippet.thumbnails.default} />
+        //     <Card.Body>
+        //       <Card.Title>{item.snippet.title}</Card.Title>
+        //       <Card.Text>
+        //         {item.snippet.description}
+        //       </Card.Text>
+        //       <Button variant="danger">Watch!</Button>
+        //     </Card.Body>
+        //   </Card>
+        //   </>) 
+        //   } 
+        //   )
+        console.log(dummySearch);
+        setVideos([...dummySearch.items]);
+        console.log(videosList);
+
+
     }
+    console.log(videosList);
+    // videosList.forEach(video => videos.current.push(video.snippet))
+    console.log(items)
 
     return  ( <><>
+    
  <Container>
       <Row>
         <Col></Col>
-        <Col xs={6}><Form onSubmit={(event)=>getVideoQuery(event, videos)}>
+        <Col xs={7}><Form onSubmit={(event)=>getVideoQuery(event, videos)}>
                 <InputGroup className="mb-3">
                 <Form.Control 
                 type="text" 
@@ -92,31 +103,30 @@ export default function Videos(){
       </Row>
       <Row>
         <Col></Col>
-        <Col xs={6}>      
-            <Alert variant="secondary" >
+        <Col xs={4}>      
+           {videosList.length === 0 ?  <Alert variant="secondary" className="text-center">
             Please submit a search keyword and hit 'Submit'.
-            </Alert>
+            </Alert> : null}
          </Col>
         <Col></Col>
       </Row>
       <Row>
         <Col></Col>
-        <Col xs={5}>            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="" />
-            <Card.Body>
-              <Card.Title>agai</Card.Title>
-              <Card.Text>
-                it blew up
-              </Card.Text>
-              <Button variant="danger">Watch!</Button>
-            </Card.Body>
-          </Card></Col>
+        <Col xs={5}>            
+     </Col>
         <Col></Col>
       </Row>
+      <Row>
+      
+      </Row>
     </Container>
+    <Container fluid>
+      <Row className="justify-content-md-center">
+<VideoCards items={items} videosList={videosList} />
+    </Row>
+  </Container>
             <Error show={modalShow} error={videosList} onHide={() => setModalShow(false)} />
     <><>
-    {videoList ? videoList : 'undefined'}
     </>
     </>
         </></>
---
+++ src/data/video.js
@@ -0,0 +1,189 @@
+const dummySearch = {
+  kind: "youtube#searchListResponse",
+  etag: "YxG6rjXuebrls__nZvAK0f9X2BI",
+  nextPageToken: "CAUQAA",
+  regionCode: "US",
+  pageInfo: {
+    totalResults: 1000000,
+    resultsPerPage: 5,
+  },
+  items: [
+    {
+      kind: "youtube#searchResult",
+      etag: "Xo_e7efdKsphAzY0qgcpbwMcpWo",
+      id: {
+        kind: "youtube#video",
+        videoId: "DP_Qlt9CSiY",
+      },
+      snippet: {
+        publishedAt: "2021-12-10T05:39:50Z",
+        channelId: "UC0BletW9phE4xHFM44q4qKA",
+        title:
+          "Juice WRLD - Feline (with Polo G &amp; Trippie Redd) [Official Audio]",
+        description:
+          'Juice WRLD "Fighting Demons" is available now: https://JuiceWrld.lnk.to/FightingDemons Subscribe to the official Juice WRLD ...',
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/DP_Qlt9CSiY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/DP_Qlt9CSiY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/DP_Qlt9CSiY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Juice WRLD",
+        liveBroadcastContent: "none",
+        publishTime: "2021-12-10T05:39:50Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "Lx7OSLeS_7ZdQ44Ld_kfkn-lxc8",
+      id: {
+        kind: "youtube#video",
+        videoId: "F6tqpv74uRY",
+      },
+      snippet: {
+        publishedAt: "2018-03-07T01:00:02Z",
+        channelId: "UCeaeJX8Q3R5pDmMBkX8yJAg",
+        title: "Cat Family - Species of Cats | Cat’s Story (Nat Geo Wild)",
+        description:
+          "We discover how the first cats arose in the forests of Asia how they spread across the continent and later came to conquer Africa.",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/F6tqpv74uRY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/F6tqpv74uRY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/F6tqpv74uRY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "WD Wildlife Documentary",
+        liveBroadcastContent: "none",
+        publishTime: "2018-03-07T01:00:02Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "1rCkvNr0qZf2aSFMultHZwyjF5Q",
+      id: {
+        kind: "youtube#video",
+        videoId: "P8uQCQhD1wo",
+      },
+      snippet: {
+        publishedAt: "2007-08-10T21:05:54Z",
+        channelId: "UCN7Zc6mHdqVvILd0yNEXhkw",
+        title: "Sesame Street - Felines",
+        description: "A song of felines and emotions.",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "tpirman1982",
+        liveBroadcastContent: "none",
+        publishTime: "2007-08-10T21:05:54Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "QMhE6tNS_BgqK7QSfv4uYv7LK6o",
+      id: {
+        kind: "youtube#video",
+        videoId: "igDp1jRsnQ8",
+      },
+      snippet: {
+        publishedAt: "2015-07-29T19:26:48Z",
+        channelId: "UCp71hnWNDBPCmJl_y2NTIdQ",
+        title: "All Cat (Feline) Species - Species List",
+        description:
+          "Feline Species List 1. Cheetah 2. Caracal 3. Black-footed cat 4. Chinese mountain cat 5. Domestic cat 6. Jungle cat 7. Sand cat 8.",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/igDp1jRsnQ8/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/igDp1jRsnQ8/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/igDp1jRsnQ8/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Gilles Delhaye",
+        liveBroadcastContent: "none",
+        publishTime: "2015-07-29T19:26:48Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "VuhAdZWwD6EfqgXBy53jqcNwJjM",
+      id: {
+        kind: "youtube#video",
+        videoId: "mIkWCQ5t7XM",
+      },
+      snippet: {
+        publishedAt: "2022-10-02T04:23:04Z",
+        channelId: "UClB2FZ7Qvwq0Ons_6jN1lAQ",
+        title: "Friends of Felines Rescue Center",
+        description:
+          "The Rescue has multiple camera streams. On this one, you can chat with the staff live as it is the main camera. Make a chat name ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/default_live.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/mqdefault_live.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/hqdefault_live.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "FOFRescueCenter",
+        liveBroadcastContent: "live",
+        publishTime: "2022-10-02T04:23:04Z",
+      },
+    },
+  ],
+};
+
+export default dummySearch;
