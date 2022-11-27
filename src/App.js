52c8eb1e33680ef225f898de2441ae124b1cda27
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-11-27 17:10:25 -0500

    Custom CSS and animations implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import { About, NavBar } from "./Components/Index";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import Videos from "./Components/videos/Videos";
+import Video from "./Components/videos/Video";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Videos />} />
           <Route path="/about" element={<About />} />
+          <Route path="/video/:id" element={<Video />} />
+          <Route path="/video/" element={<Video />} />
         </Routes>
       </BrowserRouter>
     </Box>
--- src/data/video.js
+++ src/data/video.js
@@ -1,11 +1,11 @@
 const dummySearch = {
   kind: "youtube#searchListResponse",
-  etag: "YxG6rjXuebrls__nZvAK0f9X2BI",
-  nextPageToken: "CAUQAA",
+  etag: "L4Rrv0E9z2dVFeX4SObTB1MWobU",
+  nextPageToken: "CAoQAA",
   regionCode: "US",
   pageInfo: {
     totalResults: 1000000,
-    resultsPerPage: 5,
+    resultsPerPage: 10,
   },
   items: [
     {
@@ -81,36 +81,73 @@ const dummySearch = {
     },
     {
       kind: "youtube#searchResult",
-      etag: "1rCkvNr0qZf2aSFMultHZwyjF5Q",
+      etag: "Q9wA9eiqckB9Y9wHaj7HzXBIbF4",
       id: {
         kind: "youtube#video",
-        videoId: "P8uQCQhD1wo",
+        videoId: "uQ2j3-kM5QE",
       },
       snippet: {
-        publishedAt: "2007-08-10T21:05:54Z",
-        channelId: "UCN7Zc6mHdqVvILd0yNEXhkw",
-        title: "Sesame Street - Felines",
-        description: "A song of felines and emotions.",
+        publishedAt: "2021-12-16T02:29:56Z",
+        channelId: "UC0BletW9phE4xHFM44q4qKA",
+        title:
+          "Juice WRLD with Polo G &amp; Trippie Redd - Feline (Official Lyric Video)",
+        description:
+          'Juice WRLD "Fighting Demons" is available now: https://JuiceWrld.lnk.to/FightingDemons Subscribe to the official Juice WRLD ...',
         thumbnails: {
           default: {
-            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/default.jpg",
+            url: "https://i.ytimg.com/vi/uQ2j3-kM5QE/default.jpg",
             width: 120,
             height: 90,
           },
           medium: {
-            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/mqdefault.jpg",
+            url: "https://i.ytimg.com/vi/uQ2j3-kM5QE/mqdefault.jpg",
             width: 320,
             height: 180,
           },
           high: {
-            url: "https://i.ytimg.com/vi/P8uQCQhD1wo/hqdefault.jpg",
+            url: "https://i.ytimg.com/vi/uQ2j3-kM5QE/hqdefault.jpg",
             width: 480,
             height: 360,
           },
         },
-        channelTitle: "tpirman1982",
+        channelTitle: "Juice WRLD",
         liveBroadcastContent: "none",
-        publishTime: "2007-08-10T21:05:54Z",
+        publishTime: "2021-12-16T02:29:56Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "VuhAdZWwD6EfqgXBy53jqcNwJjM",
+      id: {
+        kind: "youtube#video",
+        videoId: "mIkWCQ5t7XM",
+      },
+      snippet: {
+        publishedAt: "2022-10-02T04:23:04Z",
+        channelId: "UClB2FZ7Qvwq0Ons_6jN1lAQ",
+        title: "Friends of Felines Rescue Center",
+        description:
+          "The Rescue has multiple camera streams. On this one, you can chat with the staff live as it is the main camera. Make a chat name ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/default_live.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/mqdefault_live.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/hqdefault_live.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "FOFRescueCenter",
+        liveBroadcastContent: "live",
+        publishTime: "2022-10-02T04:23:04Z",
       },
     },
     {
@@ -150,40 +187,145 @@ const dummySearch = {
     },
     {
       kind: "youtube#searchResult",
-      etag: "VuhAdZWwD6EfqgXBy53jqcNwJjM",
+      etag: "l1heCIvhYENR6PMetGMRTglbqd4",
       id: {
         kind: "youtube#video",
-        videoId: "mIkWCQ5t7XM",
+        videoId: "RAouNlhOuSk",
       },
       snippet: {
-        publishedAt: "2022-10-02T04:23:04Z",
-        channelId: "UClB2FZ7Qvwq0Ons_6jN1lAQ",
-        title: "Friends of Felines Rescue Center",
+        publishedAt: "2021-04-13T00:24:27Z",
+        channelId: "UCikj0-7SgAPVROjxNcIo5cA",
+        title: "Felines",
         description:
-          "The Rescue has multiple camera streams. On this one, you can chat with the staff live as it is the main camera. Make a chat name ...",
+          "Provided to YouTube by DistroKid Felines · Front 242 Front By Front ℗ RRE Records Released on: 2014-01-01 Auto-generated ...",
         thumbnails: {
           default: {
-            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/default_live.jpg",
+            url: "https://i.ytimg.com/vi/RAouNlhOuSk/default.jpg",
             width: 120,
             height: 90,
           },
           medium: {
-            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/mqdefault_live.jpg",
+            url: "https://i.ytimg.com/vi/RAouNlhOuSk/mqdefault.jpg",
             width: 320,
             height: 180,
           },
           high: {
-            url: "https://i.ytimg.com/vi/mIkWCQ5t7XM/hqdefault_live.jpg",
+            url: "https://i.ytimg.com/vi/RAouNlhOuSk/hqdefault.jpg",
             width: 480,
             height: 360,
           },
         },
-        channelTitle: "FOFRescueCenter",
-        liveBroadcastContent: "live",
-        publishTime: "2022-10-02T04:23:04Z",
+        channelTitle: "Front 242 - Topic",
+        liveBroadcastContent: "none",
+        publishTime: "2021-04-13T00:24:27Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "K1osjMSTetn3oWEnyjvwilifUQo",
+      id: {
+        kind: "youtube#video",
+        videoId: "W86cTIoMv2U",
+      },
+      snippet: {
+        publishedAt: "2018-01-05T18:00:03Z",
+        channelId: "UCCj956IF62FbT7Gouszaj9w",
+        title: "World&#39;s smallest cat 🐈- BBC",
+        description:
+          "Subscribe and to the BBC https://bit.ly/BBCYouTubeSub Watch the BBC first on iPlayer https://bbc.in/iPlayer-Home ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/W86cTIoMv2U/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/W86cTIoMv2U/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/W86cTIoMv2U/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "BBC",
+        liveBroadcastContent: "none",
+        publishTime: "2018-01-05T18:00:03Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "zv_Lv78Mi2-uvVsb7Oo_IUR1A-Y",
+      id: {
+        kind: "youtube#video",
+        videoId: "3SgeGP8ykvQ",
+      },
+      snippet: {
+        publishedAt: "2020-11-08T11:00:00Z",
+        channelId: "UC8D_NXFBh8A9KSwo7x-wFdQ",
+        title: "Ultimate Wild Felines Collection 8K ULTRA HD / 8K TV",
+        description:
+          "Ultimate Wild Felines Collection 8K ULTRA HD / 8K TV High Quality Animals from the World Video In Exceptional 8K HDR 60FPS ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/3SgeGP8ykvQ/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/3SgeGP8ykvQ/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/3SgeGP8ykvQ/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "8K VIDEOS ULTRA HD",
+        liveBroadcastContent: "none",
+        publishTime: "2020-11-08T11:00:00Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "LXaIhuc0HBFgjX_SPBZAxHXkIZU",
+      id: {
+        kind: "youtube#video",
+        videoId: "QadUonunflw",
+      },
+      snippet: {
+        publishedAt: "2022-02-11T14:00:11Z",
+        channelId: "UCQtW2oz8ec8pHjjxawujNjg",
+        title:
+          "Follow Your Cat - What Felines get up to When They Leave the House | Free Documentary Nature",
+        description:
+          "Follow Your Cat - What Felines get up to When They Leave the House | Nature Documentary Watch 'Wildlife Takeover: How ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/QadUonunflw/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/QadUonunflw/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/QadUonunflw/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Free Documentary - Nature",
+        liveBroadcastContent: "none",
+        publishTime: "2022-02-11T14:00:11Z",
       },
     },
   ],
 };
-
 export default dummySearch;
