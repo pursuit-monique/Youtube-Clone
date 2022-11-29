346a3204d72f42027798475aa5942b5808475c3f
jasn4real <tobeobi@pursuit.org>
2022-11-28 22:43:57 -0500

    AboutPage without CSS

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -14,7 +14,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import VideoCards from './VideoCards'
 import Error from '../error/Error.jsx';
-import dummySearch from '/Users/moniquecorrea/pursuit/module-3/youtube-clone/src/data/video.js';
+import dummySearch from '../../data/video.js';
 
 
 export default function Videos(){
