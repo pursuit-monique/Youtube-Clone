16959b456a7852a8c0dafe89a9ef0b3715615f63
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-11-29 20:21:59 -0500

    Reconciling branch names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { About, NavBar } from "./components/Index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import NavBar from "./components/NavBar";
+import About from "./components/pages/About";
 import Videos from "./components/videos/Videos";
 import Video from "./components/videos/Video";
 
