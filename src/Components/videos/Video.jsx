fcbb309298f9f2cdbcff12b017cba3fe7afc0337
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-12-04 11:08:07 -0500

    view page complete

--- src/Components/videos/Video.jsx
+++ src/Components/videos/Video.jsx
@@ -1,6 +1,27 @@
 import {useParams} from 'react-router-dom';
+import React from 'react';
+import YouTube from 'react-youtube';
 
 export default function Video (){
     let {id} = useParams();
-    return <div>Video ID: {id}</div>
+
+
+ function video(id){
+  
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+    function onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+      }
+    return <YouTube videoId={id} opts={opts} onReady={(event) => onReady(event)} />;
+  }
+
+    return <div>{video(id)}</div>
 }
